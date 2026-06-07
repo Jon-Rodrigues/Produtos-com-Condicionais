9566577284aaadc703a6af5e621781f2f44d52f3
Jon's <jon.rod2405@gmail.com>
2026-06-07 23:09:27 +0000

    feat: Cria arquivo MVC

---
+++ ProdutosComCondicionais/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosComCondicionais.Models;
+
+namespace ProdutosComCondicionais.Controllers;
+
+public class ProdutoController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.Produtos = new List<string>
+        {
+            "Cinto de utilidade","Laço da verdade","Lançador de teia","Anel lanterna verde","Kriptonita"
+        };
+
+        ViewBag.ProdutosValor = new List<double>
+        {
+            "R$5","R$30","R$5 milhões","R$20","R$15"
+        };
+    
+        return View();
+    }
---
+++ ProdutosComCondicionais/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProdutosComCondicionais.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
